a2120d66d08f20693b3266d95eb0c65a850f03f5
Jorge <jorgesilva.ti@hotmail.com>
2023-02-22 21:32:41 -0300

    build: add postgres container and connection

--- web/web/settings.py
+++ web/web/settings.py
@@ -74,12 +74,14 @@ WSGI_APPLICATION = 'web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DB = os.getenv("DB_NAME") + '_db.sqlite3'
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / DB,
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
 
