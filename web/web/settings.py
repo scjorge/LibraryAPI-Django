dbc0c5ad5b04690c73cc596500e2b469a61f3133
Jorge <jorgesilva.ti@hotmail.com>
2023-02-23 22:12:48 -0300

    build: add profile to docker compose for multiples databases - dev and prod environment

--- web/web/settings.py
+++ web/web/settings.py
@@ -82,11 +82,11 @@ AUTH_USER_MODEL = 'user.UserProfile'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'PORT': os.environ.get('POSTGRES_PORT'),
+        'HOST': os.environ.get('POSTGRES_HOST'),
     }
 }
 
