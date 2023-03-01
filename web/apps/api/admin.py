761c082767bd36eb32537e3740501960f2f44970
Jorge <jorgesilva.ti@hotmail.com>
2023-03-01 22:56:44 +0000

    feat: add book to admin panel

--- web/apps/api/admin.py
+++ web/apps/api/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Author ,Category, Publisher
+from .models import Author, Book, Category, Publisher
 
 
 admin.site.register(Author)
+admin.site.register(Book)
 admin.site.register(Category)
 admin.site.register(Publisher)
